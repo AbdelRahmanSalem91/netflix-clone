d8e8dd3a4b79bd8a6faf405149f21c563e31aea3
AbdelRahmanSalem91 <abdoahmed424@gmail.com>
2021-11-07 23:49:39 +0200

    init the project

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyD0FOULgKcEXAVH5UZwWcnlAoVwaT1SqmU",
+  authDomain: "netflix-clone-75ac6.firebaseapp.com",
+  projectId: "netflix-clone-75ac6",
+  storageBucket: "netflix-clone-75ac6.appspot.com",
+  messagingSenderId: "848471857618",
+  appId: "1:848471857618:web:fe357f1482c4b5c8c5100b",
+};
